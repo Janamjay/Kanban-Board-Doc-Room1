53526ea01b36831b53f4d68a27a92e1d7a9d88b8
Neha-rajbhar <nr470245@gmail.com>
2023-05-14 08:59:00 +0530

    details ui complete

--- karban/src/components/activity/Activity.js
+++ karban/src/components/activity/Activity.js
@@ -5,8 +5,20 @@ import { RxActivityLog } from "react-icons/rx";
 
 function Activity() {
   const [details, setShowDetails] = useState(false);
+  const [comment,setComment]=useState(false)
   function handleButton() {
     setShowDetails(!details);
+  }
+
+  function handleComment(){
+setComment(true)
+  }
+
+  function handleInput(){
+
+  }
+  function handleClick(){
+    setComment(false)
   }
   return (
     <>
@@ -24,16 +36,41 @@ function Activity() {
         </div>
         <div className={style.comment}>
             <span className={style.justifyImage}>
-            <img className={style.userImage} src=" https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" width="50px" height="50px"/>
+            <img className={style.userImage} alt="user" src=" https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" width="40px" height="40px"/>
             </span>
             <span className={style.commentBtn}>
-                <button>Comment</button>
+              {comment?
+              
+            <>
+            <span className={style.textEditor}>
+            <input type="text" onChange={handleInput}/>
+            <div className={style.btns}>
+            <span><button className={style.save}>Save</button></span>
+            <span><button onClick={handleClick} className={style.cancel}>Cancel</button></span>
+            </div>
+           
+            </span>
+           
+            </>
+            :
+            <button onClick={handleComment}>Write a comment....</button>
+            }
+                
             </span>
         </div>
         <div className={style.detailsDiv}>
         {details ? (
           <>
-            <button className={style.hides}>HIIIIIIIIIIII</button>
+            {/* <button className={style.hides}>HIIIIIIIIIIII</button> */}
+            <div className={style.mainActivityBox}>
+              <span className={style.activityImage}>
+              <img className={style.userImages} alt="user" src=" https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" width="40px" height="40px"/> 
+              </span>
+              <span className={style.activityText}>
+                <p>Neha Rajbhar</p>
+                <p>Time and Dtae</p>
+              </span>
+            </div>
           </>
         ) : (
           <></>
