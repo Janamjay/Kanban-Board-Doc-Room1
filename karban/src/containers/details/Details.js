29f57d8c1d679945d48508cceb9467b8074819a2
Janammjay <janammjayk6@gmail.com>
2023-05-16 23:46:37 +0530

    routing

--- karban/src/containers/details/Details.js
+++ karban/src/containers/details/Details.js
@@ -7,7 +7,6 @@ import Activity from "../../components/activity/Activity";
 import Description from "../../components/description/Description";
 import { useNavigate } from "react-router";
 
-
 function Details() {
   const [input, setInput] = useState("hii");
   const navigate = useNavigate();
@@ -15,31 +14,32 @@ function Details() {
     setInput(e.target.value);
   }
 
-
   return (
-        <div className={style.windows}>
-          <div className={style.textAreaSection}>
-            <span className={style.laptop}>
-              <Icons icon={<FaLaptop />} />
-            </span>
-            <span className={style.textArea}>
-              <input
-                type="text"
-                value={input}
-                onChange={handleInput}
-                style={{ width: "90%", border: "none", height: "30px" }}
-              />
-              <p className={style.p}>in list {input}</p>
-            </span>
-            <span className={style.cross}>
-              <Icons  icon={<RxCross2  onClick={()=>navigate("/")}/>} />
-            </span>
-          </div>
-          <div>
-            <Description />
-            <Activity />
-          </div>
+    <div className={style.main}>
+      <div className={style.windows}>
+        <div className={style.textAreaSection}>
+          <span className={style.laptop}>
+            <Icons icon={<FaLaptop />} />
+          </span>
+          <span className={style.textArea}>
+            <input
+              type="text"
+              value={input}
+              onChange={handleInput}
+              style={{ width: "90%", border: "none", height: "30px" }}
+            />
+            <p className={style.p}>in list {input}</p>
+          </span>
+          <span className={style.cross}>
+            <Icons icon={<RxCross2 onClick={() => navigate("/")} />} />
+          </span>
         </div>
+        <div>
+          <Description />
+          <Activity />
+        </div>
+      </div>
+    </div>
   );
 }
 export default Details;
